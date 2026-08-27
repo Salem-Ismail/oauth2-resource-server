fe241cc6c3699c216248aa9214e7b8667c69aca2
Salem-Ismail <181155140+Salem-Ismail@users.noreply.github.com>
2026-08-26 23:46:02 -0400

    Add controller tests for user status endpoint

--- src/main/java/com/scotia/resource_server/HelloController.java
+++ src/main/java/com/scotia/resource_server/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Salem at Scotiabank!";
+        return "Resource server is up.";
     }
 
 }
